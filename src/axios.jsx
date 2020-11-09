eb6d4f89379d51ae966cbc96613e1896f862d3bb
gabriel-dev-96 <gabriel1996ca@gmail.com>
2020-11-08 21:47:53 -0500

    make it better

---
+++ src/axios.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+/**base url to make requests to the movie database */
+
+const instance = axios.create({
+    baseURL: "http://api.themoviedb.org/3",
+});
+
+//instance.get('/foo-bar');
+
+// http://api.themoviedb.org
+
+export default instance;
